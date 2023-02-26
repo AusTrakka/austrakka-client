4ef08d1e41219eece5c23e2bdab8dc3ba5385fb9
Jolene Farrell <jolenef@5260L-152789-M.local>
2023-02-27 09:48:26 +1100

    Generate tabs and pass stateful props

--- src/components/ProjectOverview/ProjectOverview.tsx
+++ src/components/ProjectOverview/ProjectOverview.tsx
@@ -42,7 +42,12 @@ const projectOverviewTabs: TabContentProps[] = [
   }
 ]
 
+//TODO: Define types for expected responses from the key endpoints and pass them to the initial states
+
+//
+
 const ProjectOverview = () => {
+  // TODO: Clear state on component unmount - for cleanliness when a user goes back and changes the project which they selected 
   const [state, updateState] = useState({
     loading: false,
     projectDetails: {},
@@ -50,39 +55,61 @@ const ProjectOverview = () => {
     projectDesc: "",
     lastUpload: ""
   })
-  const [projectSubmissions, setProjectSubmissions] = useState()
+  const [projectSubmissions, setProjectSubmissions] = useState([])
   const [projectAnalyses, setProjectAnalyses] = useState()
-  const [projectDetails, setProjectDetails] = useState()
+  const [projectDetails, setProjectDetails] = useState({description: ""})
 
-  //Get project name and details from localStorage item (selected project Id)
   useEffect(() => {
-    getProject()
+    getProject() //API calls
   }, [])
+
+  useEffect(() => {
+    populateTabComponents() //pushing stateful props based on API response
+  }, [projectDetails, projectAnalyses, projectSubmissions, state]) 
 
   async function getProject() {
     // TODO: Get project details (/api/Projects/id) based on project id rather than session storage 
     await getProjectDetails() 
-    .then((response) => response.json())
-    .then((response_data) => { 
-      setProjectDetails(response_data)
-    })
-    .catch(error => console.log(error))
+      .then((response) => response.json())
+      .then((response_data) => {
+        setProjectDetails(response_data.data)
+      })
+      .catch(error => console.log(error))
 
     // Get submissions 
     await getSubmissions()
       .then((response) => response.json())
-      .then((response_data) => { 
-        setProjectSubmissions(response_data) 
+      .then((response_data) => {
+        setProjectSubmissions(response_data)
+        // Get latest upload date
+        let latestDate = new Date(
+          Math.max(
+            ...response_data.map((element:any) => {
+              let formattedDate = ((new Date (element.created)))
+              return formattedDate
+            })
+          )
+        )
+        updateState({...state, lastUpload: latestDate.toDateString()})
       })
       .catch(error => console.log(error))
     
     // Get analyses  
     await getAnalyses()
-    .then((response) => response.json())
-    .then((response_data) => { setProjectAnalyses(response_data) })
-    .catch(error => console.log(error)) 
+      .then((response) => response.json())
+      .then((response_data) => {
+        setProjectAnalyses(response_data)
+      })
+      .catch(error => console.log(error))
+  }
 
-    //updateState({...state, projectDesc: projectDetails.data.description, totalSamples: 9})
+  function populateTabComponents() {
+    projectOverviewTabs.forEach((tab) => {
+      if (tab.title == "Summary") {tab.component = <Summary samples={projectSubmissions.length} lastUpload={state.lastUpload} projectDesc={projectDetails.description} />}
+      if (tab.title == "Samples") {tab.component = <Samples />}
+      if (tab.title == "Trees") {tab.component = <TreeList />}
+      if (tab.title == "Plots") {tab.component = <Plots />}
+    })
   }
   
   return (
