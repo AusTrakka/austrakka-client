3287cf9860874e1156b54b6674014fa81e6a75ef
Jolene Farrell <jolenef@5260L-152789-M.local>
2023-03-22 14:58:47 +1100

    Placeholder alert if login is unsuccessful

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Button } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Button, Alert } from '@mui/material';
 import { useMsal } from '@azure/msal-react';
 import styles from './Login.module.css';
 import AusTrakkaLogo from '../../assets/logos/AusTrakka_Logo_cmyk.png';
@@ -8,16 +8,25 @@ import { loginRequest } from '../../config/authConfig';
 // TODO: Add login loading and login failure features
 function LoginButton() {
   const { instance } = useMsal();
+  const [loginError, setLoginError] = useState(false);
 
   const handleLogin = (loginType: string) => {
     if (loginType === 'popup') {
-      instance.loginPopup(loginRequest).catch((error) => {
-        console.log(error);
+      instance.loginPopup(loginRequest).catch(() => {
+        setLoginError(true);
       });
     }
   };
   return (
-    <Button variant="contained" onClick={() => handleLogin('popup')}>Log in</Button>
+    <>
+      <Button variant="contained" onClick={() => handleLogin('popup')}>Log in</Button>
+      { loginError
+        ? (
+          <Alert severity="error" sx={{ m: 2, textAlign: 'left' }}>
+            There has been an error logging you in to AusTrakka, please try again later.
+          </Alert>
+        ) : null }
+    </>
   );
 }
 
