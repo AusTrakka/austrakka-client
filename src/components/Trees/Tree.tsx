b7118cd6639ad337e5891de59c5f5485272016e8
Wytamma Wirth <wytamma.wirth@me.com>
2023-06-15 02:25:28 +1000

    :sparkles: add export functions

--- src/components/Trees/Tree.tsx
+++ src/components/Trees/Tree.tsx
@@ -1,47 +1,75 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState, useImperativeHandle } from 'react';
 import { Phylocanvas, PhylocanvasProps } from './PhylocanvasGL';
 
 export interface TreeProps extends PhylocanvasProps {
   resizeWidthTo: string | null;
-  size: { width: number; height: number };
+  onSelectedIdsChange: CallableFunction
 }
 
-function Tree({ resizeWidthTo, ...otherProps }: TreeProps) {
-  const treeDiv = useRef<HTMLDivElement>(null);
-  const tree = useRef<any | null>(null);
-  const [size, setSize] = useState<{ width: number; height: number }>(otherProps.size);
+export type TreeExportFuctions = {
+  exportNewick(): string;
+  exportSVG(): Blob;
+  exportPNG(): Blob;
+  exportJSON(): string;
+};
 
-  useEffect(() => {
-    function handleResize() {
-      const gridRef = document.querySelector(resizeWidthTo!);
-      const width = gridRef?.getBoundingClientRect().width!;
-      if (width) {
-        setSize({ height: size.height, width: width - 40 });
+const Tree = React.forwardRef(
+  ({ resizeWidthTo, onSelectedIdsChange, ...otherProps }: TreeProps, ref) => {
+    const treeDiv = useRef<HTMLDivElement>(null);
+    const tree = useRef<Phylocanvas | null>(null);
+    const [size, setSize] = useState<{ width: number; height: number }>(otherProps.size);
+
+    useImperativeHandle(ref, () => ({
+      exportNewick() {
+        return tree.current?.exportNewick();
+      },
+      exportSVG() {
+        return tree.current?.exportSVG();
+      },
+      exportPNG() {
+        return tree.current?.exportPNG();
+      },
+      exportJSON() {
+        return tree.current?.exportJSON();
+      },
+    }));
+
+    useEffect(() => {
+      function handleResize() {
+        const gridRef = document.querySelector(resizeWidthTo!);
+        const width = gridRef?.getBoundingClientRect().width!;
+        if (width) {
+          setSize({ height: size.height, width: width - 40 });
+        }
       }
-    }
-    if (tree.current == null && treeDiv.current !== null) {
+      if (tree.current == null && treeDiv.current !== null) {
+        if (resizeWidthTo !== null) {
+          handleResize();
+        }
+        tree.current = new Phylocanvas(treeDiv.current, { ...otherProps });
+        tree.current.addClickHandler(() => {
+        // save the selectedIds in the state
+          onSelectedIdsChange(tree.current?.props.selectedIds);
+        });
+      }
       if (resizeWidthTo !== null) {
-        handleResize();
+        window.addEventListener('resize', handleResize);
       }
-      tree.current = new Phylocanvas(treeDiv.current, { ...otherProps });
-    }
-    if (resizeWidthTo !== null) {
-      window.addEventListener('resize', handleResize);
-    }
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [otherProps, resizeWidthTo, size]);
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, [onSelectedIdsChange, otherProps, resizeWidthTo, size]);
 
-  useEffect(() => {
-    const updatedProps = { ...otherProps, size };
-    if (tree.current) {
+    useEffect(() => {
+      const updatedProps = { ...otherProps, size };
+      if (tree.current) {
       // update the props on change
-      tree.current.setProps({ ...updatedProps });
-    }
-  }, [otherProps, size]);
+        tree.current.setProps({ ...updatedProps });
+      }
+    }, [otherProps, size]);
 
-  return <div ref={treeDiv} />;
-}
+    return <div ref={treeDiv} />;
+  },
+);
 
 export default Tree;
