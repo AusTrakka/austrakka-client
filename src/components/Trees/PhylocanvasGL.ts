22259289bd848df76dff42e148f2f1dd094f66b6
Wytamma Wirth <wytamma.wirth@me.com>
2023-06-15 00:30:24 +1000

    :sparkles: add clickhandlers to phylocanvas

--- src/components/Trees/PhylocanvasGL.ts
+++ src/components/Trees/PhylocanvasGL.ts
@@ -1,11 +1,18 @@
-import { PhylocanvasGL, plugins } from '@phylocanvas/phylocanvas.gl';
+import { PhylocanvasGL, plugins, TreeTypes } from '@phylocanvas/phylocanvas.gl';
 
 type MetadataValue = { 'colour': string, 'label': string };
 export type TreeMetadata = Record<string, Record<string, MetadataValue>>;
 
+interface Size {
+  width: number;
+  height: number;
+}
+
+type TreeType = typeof TreeTypes[keyof typeof TreeTypes];
+
 export interface PhylocanvasProps {
   source: string;
-  size: { width: number; height: number };
+  size: Size;
   showLabels: boolean;
   showLeafLabels: boolean;
   interactive: boolean;
@@ -14,6 +21,49 @@ export interface PhylocanvasProps {
   alignLabels: boolean;
   showBlockHeaders: boolean;
   nodeSize: number;
+  backgroundColour?: null | string;
+  blockHeaderFontSize?: number;
+  blockPadding?: number;
+  blockSize?: number;
+  branchLengthsDigits?: number;
+  branchLengthsFormat?: null | string;
+  branchZoom?: number;
+  centre?: [number, number];
+  collapsedIds?: any[]; // Replace any[] with the actual type if known
+  edgeOverlapFactor?: number;
+  fillColour?: [number, number, number, number];
+  fontColour?: [number, number, number, number];
+  fontFamily?: string;
+  fontSize?: number;
+  haloRadius?: number;
+  haloWidth?: number;
+  highlightColour?: [number, number, number, number];
+  minScale?: number;
+  nodeOverlapFactor?: number;
+  nodeShape?: string;
+  padding?: number;
+  rootId?: null | string;
+  rotatedIds?: any[]; // Replace any[] with the actual type if known
+  scaleLineAlpha?: boolean;
+  selectedIds?: any[]; // Replace any[] with the actual type if known
+  shapeBorderAlpha?: number;
+  shapeBorderWidth?: number;
+  // showBlockLabels?: boolean;
+  showBranchLengths?: boolean;
+  showEdges?: boolean;
+  showInternalLabels?: boolean;
+  showPiecharts?: boolean;
+  showShapeBorders?: boolean;
+  showShapes?: boolean;
+  stepZoom?: number;
+  strokeColour?: [number, number, number, number];
+  strokeWidth?: number;
+  styleLeafLabels?: boolean;
+  styleNodeEdges?: boolean;
+  styles?: Record<string, any>; // An object containing node styles keyed by leaf labels.
+  treeToCanvasRatio?: number;
+  type?: TreeType;
+  zoom?: number;
 }
 
 export class Phylocanvas extends PhylocanvasGL {
@@ -23,7 +73,17 @@ export class Phylocanvas extends PhylocanvasGL {
       props,
       [plugins.scalebar],
     );
+    this.clickHandlers = [];
     this.view.style.backgroundImage = ''; // remove logo
+  }
+
+  addClickHandler(fn: Function) {
+    this.clickHandlers.push(fn);
+  }
+
+  handleClick(info: any, event: any) {
+    super.handleClick(info, event);
+    this.clickHandlers.forEach((fn: Function) => fn(info, event));
   }
   // add additional methods here...
 }
