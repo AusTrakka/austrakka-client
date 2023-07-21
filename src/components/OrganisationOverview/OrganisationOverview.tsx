f0b3719f1c1282714789a5f4a735772a4ec18dec
Jolene Farrell <jolene.farrell@unimelb.edu.au>
2023-07-21 10:28:17 +1000

    :construction: Work in progress

--- src/components/OrganisationOverview/OrganisationOverview.tsx
+++ src/components/OrganisationOverview/OrganisationOverview.tsx
@@ -1,8 +1,15 @@
-import React, {
-  memo, useEffect, useState,
-} from 'react';
+import React, { memo, useEffect, useState } from 'react';
 import { Box, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from '@mui/material';
-import { ResponseObject, getGroupList } from '../../utilities/resourceUtils';
+import { MRT_ColumnDef, MRT_PaginationState, MRT_SortingState } from 'material-react-table';
+import { ResponseObject, getDisplayFields, getGroupList } from '../../utilities/resourceUtils';
+import OrganisationSamples from './OrganisationSamples';
+import Samples from '../ProjectOverview/Samples';
+import LoadingState from '../../constants/loadingState';
+import { DisplayFields } from '../../types/fields.interface';
+import { Filter } from '../Common/QueryBuilder';
+import { ProjectSample } from '../../types/sample.interface';
+import { MetaDataColumn } from '../../types/dtos';
+import isoDateLocalDate, { isoDateLocalDateNoTime } from '../../utilities/helperUtils';
 
 function OrgGroupSelector(props: any) {
   const { selectedGroup, setSelectedGroup, groups } = props;
@@ -18,16 +25,17 @@ function OrgGroupSelector(props: any) {
         label="Organisation group"
         autoWidth
       >
-        { // Need to add a check in here to account for an empty list of groups (and below)
-            groups.map((group: any) => (
-              <MenuItem
-                value={group.name}
-                key={group.name}
-              >
-                {group.name}
-              </MenuItem>
-            ))
-        }
+        { groups.map((group: any) => (
+          <MenuItem
+            value={group}
+            key={group.name}
+          >
+            {group.name}
+          </MenuItem>
+        )) }
+        { groups.length === 0 ? (
+          <MenuItem disabled>No owner groups available</MenuItem>
+        ) : null}
       </Select>
     </FormControl>
   );
@@ -37,22 +45,60 @@ function OrganisationOverview() {
   const [selectedGroup, setSelectedGroup] = useState('');
   const [groups, setGroups] = useState([]);
 
+  // Below is a cut and paste from ProjectOverview.tsx
+  const [sampleTableColumns, setSampleTableColumns] = useState<MRT_ColumnDef[]>([]);
+  const [sorting, setSorting] = useState<MRT_SortingState>([]);
+  const [samplesPagination, setSamplesPagination] = useState<MRT_PaginationState>({
+    pageIndex: 0,
+    pageSize: 50,
+  });
+  const [columnOrderArray, setColumnOrderArray] = useState<string[]>([]);
+  const [isSamplesLoading, setIsSamplesLoading] = useState(false);
+  const [projectSamples, setProjectSamples] = useState<ProjectSample[]>([]);
+  const [totalSamples, setTotalSamples] = useState(0);
+  const [samplesCount, setSamplesCount] = useState(0);
+  const [isSamplesError, setIsSamplesError] = useState({
+    samplesHeaderError: false,
+    sampleMetadataError: false,
+    samplesErrorMessage: '',
+  });
+  const [isFiltersOpen, setIsFiltersOpen] = useState(false);
+  const [queryString, setQueryString] = useState('');
+  const [filterList, setFilterList] = useState<Filter[]>([]);
+  const [displayFields, setDisplayFields] = useState<DisplayFields[]>([]);
+  const [exportCSVStatus, setExportCSVStatus] = useState<LoadingState>(LoadingState.IDLE);
+  const [exportData, setExportData] = useState<ProjectSample[]>([]);
+  ///
+
   async function getGroups() {
     const groupResponse: ResponseObject = await getGroupList();
     if (groupResponse.status === 'Success') {
-      setGroups(groupResponse.data);
+      // Filter out only owner groups
+      const ownerGroups = groupResponse.data.filter((group: any) => group.name.includes('-Owner'));
+      setGroups(ownerGroups);
+      if (ownerGroups.length) {
+        setSelectedGroup(ownerGroups[0]);
+      }
     } else {
+      // TODO: Set error message here - endpoint error message
       console.log(groupResponse);
     }
   }
-  useEffect(
-    () => {
-      getGroups();
-    },
-    [],
-  );
-  // TODO:
-  // - Once group is selected, get display fields and relevant samples
+
+  useEffect(() => {
+    getGroups();
+  }, []);
+  useEffect(() => {
+    // 1: GET TABLE HEADERS & FILTER FIELDS - can be custom hook
+    // 2: GET COLUMN ORDER - an be custom hook
+    // 3: GET SAMPLES
+    // 4: GET EXPORT DATA
+
+  }, [selectedGroup]);
+
+  const getExportData = async () => {
+  };
+
   return (
     <Box>
       <Grid container direction="row" spacing={2}>
@@ -64,13 +110,31 @@ function OrganisationOverview() {
             setSelectedGroup={setSelectedGroup}
           />
         </Grid>
-        <Grid container item>
-          Query builder to go here
-        </Grid>
-        <Grid container item>
-          Table to go here
-        </Grid>
       </Grid>
+      <Samples
+        totalSamples={totalSamples}
+        samplesCount={samplesCount}
+        sampleList={projectSamples}
+        isSamplesLoading={isSamplesLoading}
+        sampleTableColumns={sampleTableColumns}
+        isSamplesError={isSamplesError}
+        sorting={sorting}
+        setSorting={setSorting}
+        samplesPagination={samplesPagination}
+        setSamplesPagination={setSamplesPagination}
+        isFiltersOpen={isFiltersOpen}
+        setIsFiltersOpen={setIsFiltersOpen}
+        setQueryString={setQueryString}
+        filterList={filterList}
+        setFilterList={setFilterList}
+        displayFields={displayFields}
+        columnOrderArray={columnOrderArray}
+        getExportData={getExportData}
+        setExportData={setExportData}
+        exportCSVStatus={exportCSVStatus}
+        setExportCSVStatus={setExportCSVStatus}
+        exportData={exportData}
+      />
     </Box>
   );
 }
