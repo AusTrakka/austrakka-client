3b3c4c9c6abb7e2133824bf6197833da404c5968
Wytamma Wirth <wytamma.wirth@me.com>
2023-07-24 23:19:49 +1000

    :art: better type cases

--- src/utilities/navigationUtils.ts
+++ src/utilities/navigationUtils.ts
@@ -8,23 +8,35 @@ export default function useQueryParams<T extends {}>(defaultState: T): Partial<T
     const [key, value] = param;
 
     if (key in defaultState) {
-      const expectedType = typeof defaultState[key as keyof T];
-
-      // Parse based on the type of the corresponding key in defaultState.
-      switch (expectedType) {
-        case 'boolean':
-          params[key] = value === 'true';
-          break;
-        case 'number':
-          params[key] = Number(value);
-          break;
-        case 'object':
-          // We assume the 'object' type corresponds to an array.
-          // Adjust this logic if your state contains other kinds of objects.
-          params[key] = value.split(',');
-          break;
-        default:
-          params[key] = value;
+      // Add case for 'null' value
+      if (value === 'null') {
+        params[key] = null;
+      } else {
+        let expectedType;
+        if (key === 'rootId') {
+          expectedType = 'string';
+        } else {
+          expectedType = typeof defaultState[key as keyof T];
+        }
+        // Parse based on the type of the corresponding key in defaultState.
+        switch (expectedType) {
+          case 'boolean':
+            params[key] = value === 'true';
+            break;
+          case 'number':
+            params[key] = Number(value);
+            break;
+          case 'string':
+            params[key] = value;
+            break;
+          case 'object':
+            // We assume the 'object' type corresponds to an array.
+            // Adjust this logic if your state contains other kinds of objects.
+            params[key] = value.split(',');
+            break;
+          default:
+            params[key] = value;
+        }
       }
     }
   }
