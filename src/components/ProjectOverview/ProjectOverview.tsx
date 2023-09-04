3071b886b354aa254f8001752b92d8e20ba38cb3
Jolene Farrell <jolene.farrell@unimelb.edu.au>
2023-09-04 12:14:00 +1000

    AB#4708 Query builder open by default

--- src/components/ProjectOverview/ProjectOverview.tsx
+++ src/components/ProjectOverview/ProjectOverview.tsx
@@ -56,7 +56,7 @@ function ProjectOverview() {
     sampleMetadataError: false,
     samplesErrorMessage: '',
   });
-  const [isFiltersOpen, setIsFiltersOpen] = useState(false);
+  const [isFiltersOpen, setIsFiltersOpen] = useState(true);
   const [queryString, setQueryString] = useState('');
   const [filterList, setFilterList] = useState<Filter[]>([]);
   const [displayFields, setDisplayFields] = useState<DisplayField[]>([]);
