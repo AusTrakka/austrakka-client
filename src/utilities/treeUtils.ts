4a9cf05ece90422759b419e9174b92149354d157
Clare Sloggett <claresloggett@gmail.com>
2023-10-17 16:57:07 +1100

    Bugfix: remove unnecessary palette re-generation; sort unique values before generating palette

--- src/utilities/treeUtils.ts
+++ src/utilities/treeUtils.ts
@@ -7,7 +7,7 @@ export default function mapMetadataToPhylocanvas(
   fieldInformation: DisplayField[],
 ) {
   // A dictionary to store the colour palettes for each metadata column
-  const metadataColumnPalettes: Record<string, string[]> = {};
+  const metadataColumnPalettes: PhylocanvasLegends = {};
 
   function generateSequentialColourPalette(baseColour: any, numberOfColours: number) {
     const colours = [];
@@ -32,26 +32,10 @@ export default function mapMetadataToPhylocanvas(
     let hash = 0;
     for (let i = 0; i < str.length; i++) {
       const char = str.charCodeAt(i);
+      // eslint-disable-next-line no-bitwise
       hash = (hash << 5) - hash + char;
     }
     return Math.abs(hash);
-  }
-
-  function sortColoursInField(field: { [colour: string]: string }): { [colour: string]: string } {
-    const fieldKeys = Object.keys(field);
-
-    // Extract and sort the values
-    const fieldValues = fieldKeys.map((colour) => field[colour]);
-    fieldValues.sort((a, b) => parseInt(a, 10) - parseInt(b, 10));
-
-    // Create a new object with the sorted values but keep the original keys
-    const sortedColours: { [colour: string]: string } = {};
-
-    fieldKeys.forEach((colour, index) => {
-      sortedColours[colour] = fieldValues[index];
-    });
-
-    return sortedColours;
   }
 
   function generateDistinctColourPalette(baseColour : any, numberOfColours: number) {
@@ -88,31 +72,34 @@ export default function mapMetadataToPhylocanvas(
 
       // Check if the value is an integer (numeric string)z
       const isNumericString = fieldInfo === 'number' || fieldInfo === 'float';
-      // Generate a harmonious colour palette for the current metadata column
-      // or distinct palette if it is not an number
-      metadataColumnPalettes[metadataColumn] = isNumericString
-        ? generateSequentialColourPalette(baseColour, dataArray.length)
-        : generateDistinctColourPalette(baseColour, dataArray.flatMap((data) =>
-          data.metadataValues
-            .filter((metadataValue) => metadataValue.key === metadataColumn)
-            .map((metadataValue) => metadataValue.value)).length);
+
+      const values = dataArray.flatMap((data) =>
+        data.metadataValues
+          .filter((metadataValue) => metadataValue.key === metadataColumn)
+          .map((metadataValue) => metadataValue.value));
+
+      const collator = new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' });
+      const uniqueValues = [...new Set(values)]
+        .sort(collator.compare);
+
+      const colours = isNumericString ?
+        generateSequentialColourPalette(baseColour, uniqueValues.length) :
+        generateDistinctColourPalette(baseColour, uniqueValues.length);
+
+      metadataColumnPalettes[metadataColumn] = {};
+      uniqueValues.forEach((val, index) => {
+        metadataColumnPalettes[metadataColumn][val] = colours[index];
+      });
     }
 
     const palette = metadataColumnPalettes[metadataColumn];
 
-    // Find the index for the current value in the metadata column
-    const index = dataArray.findIndex((data) =>
-      data.metadataValues.some((metadataValue) =>
-        metadataValue.key === metadataColumn && metadataValue.value === value));
-
-    const colour = palette[index % palette.length] || 'rgba(0,0,0,0)';
+    const colour = palette[value] || 'rgba(0,0,0,0)';
 
     return colour;
   }
 
   const result: PhylocanvasMetadata = {};
-  const legends: PhylocanvasLegends = {};
-  const sortedLegends: PhylocanvasLegends = {};
   for (const data of dataArray) {
     result[data.sampleName] = {};
 
@@ -128,24 +115,13 @@ export default function mapMetadataToPhylocanvas(
           colour: uColour,
           label: metadataValue.value,
         };
-        if (metadataValue.value !== null) {
-          if (!legends[metadataValue.key]) {
-            legends[metadataValue.key] = {}; // Initialize if it doesn't exist
-          }
-
-          legends[metadataValue.key][uColour] = metadataValue.value;
-        }
       }
     }
   }
 
-  for (const field of Object.keys(legends)) {
-    sortedLegends[field] = sortColoursInField(legends[field]);
-  }
-
   const metadataAndLegends = {
     result,
-    sortedLegends,
+    legends: metadataColumnPalettes,
   };
   return metadataAndLegends;
 }
