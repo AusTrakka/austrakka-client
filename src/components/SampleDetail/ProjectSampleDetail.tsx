1339f99a4c6d059bbab47943d3f86d016279e797
Clare Sloggett <claresloggett@gmail.com>
2024-02-21 17:58:59 +1100

    AB#5230 Use project redux metadata state for project SampleDetail page; dispatch fetch request from this page; use Skeleton component to indicate still-loading fields.

--- src/components/SampleDetail/ProjectSampleDetail.tsx
+++ src/components/SampleDetail/ProjectSampleDetail.tsx
@@ -1,88 +1,59 @@
-import { Alert, Paper, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material';
+import { Alert, Paper, Skeleton, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-import { MetaDataColumn, Project } from '../../types/dtos';
+import { Field } from '../../types/dtos';
 import { Sample } from '../../types/sample.interface';
-import { getDisplayFields, getProjectDetails, getSamples } from '../../utilities/resourceUtils';
 import { SAMPLE_ID_FIELD } from '../../constants/metadataConsts';
 import { useApi } from '../../app/ApiContext';
 import LoadingState from '../../constants/loadingState';
-import { ResponseObject } from '../../types/responseObject.interface';
-import { ResponseType } from '../../constants/responseType';
 import { renderValue } from '../../utilities/helperUtils';
+import { ProjectMetadataState, fetchProjectMetadata, selectAwaitingProjectMetadata, selectProjectMetadata } from '../../app/projectMetadataSlice';
+import { useAppDispatch, useAppSelector } from '../../app/store';
 
 function SampleDetail() {
   const { projectAbbrev, seqId } = useParams();
-  const [project, setProject] = useState<Project | null>();
-  const [displayFields, setDisplayFields] = useState<MetaDataColumn[]>([]);
-  const [data, setData] = useState<Sample | null>();
   const [colWidth, setColWidth] = useState<number>(100);
-  const [errMsg, setErrMsg] = useState<string | null>(null);
   const { token, tokenLoading } = useApi();
+  const dispatch = useAppDispatch();
+
+  // Would there be any benefit in more refined selectors which select out sample?
+  const projectMetadata : ProjectMetadataState | null =
+    useAppSelector(state => selectProjectMetadata(state, projectAbbrev));
+  const awaitingMetadata : boolean = useAppSelector((state) =>
+    selectAwaitingProjectMetadata(state, projectAbbrev));
+  // If not awaiting metadata, at least one view and therefore Seq_ID should be loaded
+  const sample = !awaitingMetadata &&
+    projectMetadata?.columnLoadingStates[SAMPLE_ID_FIELD] === LoadingState.SUCCESS ?
+    projectMetadata?.metadata?.find((s: Sample) => s[SAMPLE_ID_FIELD] === seqId) :
+    null;
 
   useEffect(() => {
-    const updateProject = async () => {
-      const projectResponse: ResponseObject = await getProjectDetails(projectAbbrev!, token);
-      if (projectResponse.status === ResponseType.Success) {
-        setProject(projectResponse.data as Project);
-      } else {
-        setErrMsg(`Project ${projectAbbrev} could not be accessed`);
-      }
-    };
-    if (tokenLoading !== LoadingState.LOADING &&
-      tokenLoading !== LoadingState.IDLE) {
-      updateProject();
+    if (projectAbbrev &&
+      tokenLoading !== LoadingState.IDLE &&
+      tokenLoading !== LoadingState.LOADING
+    ) {
+      dispatch(fetchProjectMetadata({ projectAbbrev, token }));
     }
-  }, [projectAbbrev, token, tokenLoading]);
+  }, [projectAbbrev, token, tokenLoading, dispatch]);
 
   useEffect(() => {
-    const updateDisplayFields = async () => {
-      const response = await getDisplayFields(project!.projectMembers.id, token);
-      if (response.status === ResponseType.Success) {
-        setDisplayFields(response.data as MetaDataColumn[]);
-      } else {
-        setErrMsg(`Metadata fields for project ${project!.abbreviation} could not be loaded`);
-      }
-    };
-    if (project &&
-      tokenLoading !== LoadingState.LOADING &&
-      tokenLoading !== LoadingState.IDLE) {
-      updateDisplayFields();
-    }
-  }, [project, token, tokenLoading]);
-
-  useEffect(() => {
-    const updateSampleData = async () => {
-      const searchParams = new URLSearchParams({
-        filters: `${SAMPLE_ID_FIELD}==${seqId}`,
-      });
-      const response = await getSamples(token, project!.projectMembers.id, searchParams);
-      if (response.status === ResponseType.Success && response.data.length > 0) {
-        setData(response.data[0] as Sample);
-      } else {
-        setErrMsg(`Record ${seqId} could not be found within the context of project ${project!.abbreviation}`);
-      }
-    };
-    if (project &&
-      tokenLoading !== LoadingState.LOADING &&
-      tokenLoading !== LoadingState.IDLE) {
-      updateSampleData();
-    }
-  }, [project, seqId, token, tokenLoading]);
-
-  useEffect(() => {
-    if (displayFields.length > 0) {
-      const longestFieldLength = displayFields
+    if (projectMetadata?.fields && projectMetadata.fields.length > 0) {
+      const longestFieldLength = projectMetadata.fields
         .map(field => field.columnName.length)
         .reduce((a, b) => Math.max(a, b));
       setColWidth(longestFieldLength);
     }
-  }, [displayFields]);
+  }, [projectMetadata?.fields]);
 
-  const renderRow = (field: MetaDataColumn, value: any) => (
+  const renderRow = (field: Field, value: any) => (
     <TableRow key={field.columnName}>
       <TableCell width={`${colWidth}em`}>{field.columnName}</TableCell>
-      <TableCell>{ renderValue(value, field.columnName, field.primitiveType ?? 'category') }</TableCell>
+      <TableCell>
+        {(projectMetadata?.columnLoadingStates[field.columnName] === LoadingState.IDLE ||
+          projectMetadata?.columnLoadingStates[field.columnName] === LoadingState.LOADING) ?
+            <Skeleton variant="text" animation="wave" width="20em" /> :
+          renderValue(value, field.columnName, field.primitiveType ?? 'category')}
+      </TableCell>
     </TableRow>
   );
 
@@ -91,7 +62,8 @@ function SampleDetail() {
       <Typography className="pageTitle">
         {`${seqId} (${projectAbbrev} project view)`}
       </Typography>
-      {errMsg ? <Alert severity="error">{errMsg}</Alert> : (
+      {projectMetadata?.errorMessage && <Alert severity="error">{projectMetadata.errorMessage}</Alert>}
+      {(sample && projectMetadata?.fields) ? (
         <>
           <Typography>
             {`Information available through project ${projectAbbrev} for record ${seqId} is listed here.`}
@@ -99,16 +71,17 @@ function SampleDetail() {
           <TableContainer component={Paper} sx={{ mt: 3 }}>
             <Table>
               <TableBody>
-                {data &&
-                displayFields
-                  .sort((a, b) => a.columnOrder - b.columnOrder)
-                  .map(field => renderRow(field, (data as any)[field.columnName]))}
+                {projectMetadata?.fields.map(
+                  field => renderRow(field, sample[field.columnName]),
+                )}
               </TableBody>
             </Table>
           </TableContainer>
         </>
-      )}
+      ) :
+        <Typography>Loading...</Typography>}
     </>
   );
 }
+
 export default SampleDetail;
