30107f291ebd57c232a299ea0a9e3bcc88aee50d
Mann Sheth <mann.sheth@unimelb.edu.au>
2024-02-27 12:31:34 +1100

    AB#5244 made conditional more explicit.

--- src/components/Layouts/MainMenuLayout.tsx
+++ src/components/Layouts/MainMenuLayout.tsx
@@ -88,8 +88,9 @@ function MainMenuLayout() {
   const location = useLocation();
   const pathnames = location.pathname.split('/').filter((x) => x);
 
-  if (pathnames.length > 1 &&
-      noBreadCrumbIfLast.some(item => pathnames[pathnames.length - 1].endsWith(item))) {
+  if (pathnames.length <= 3 &&
+      noBreadCrumbIfLast.some(item => pathnames[pathnames.length - 1].endsWith(item))
+      && (pathnames[0] === 'projects' || pathnames[0] === 'org')) {
     pathnames.pop();
   }
 
