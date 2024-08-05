e85e54b02b7babfdc18f88b28ccd48bf5fe5102f
Clare Sloggett <claresloggett@gmail.com>
2024-08-05 11:23:19 +1000

    AB#5936 Navigate home when the user clicks the AusTrakka logo in the top left

--- src/components/Layouts/MainMenuLayout.tsx
+++ src/components/Layouts/MainMenuLayout.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import React, { useEffect, useState } from 'react';
 import {
-  NavLink, useLocation, Link, Outlet,
+  NavLink, useLocation, Link, Outlet, useNavigate,
 } from 'react-router-dom';
 import {
   Inventory, Upload, Help,
@@ -69,6 +69,7 @@ const pages = [
 ];
 
 function MainMenuLayout() {
+  const navigate = useNavigate();
   const [pageStyling, updatePageStyling] = useState('pagePadded');
   const [drawer, setDrawer] = useState(true);
   const breadcrumbNameMap: { [key: string]: any } = {
@@ -165,7 +166,7 @@ function MainMenuLayout() {
           }}
         >
           <Box sx={{ display: 'flex', flexDirection: drawer ? 'row' : 'column', justifyContent: 'center' }}>
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Box sx={{ display: 'flex', justifyContent: 'center' }} onClick={() => navigate('/')}>
               {drawer ? (<img src={AusTrakkaLogo} alt="logo" className={styles.logo} />) : <img src={AusTrakkaLogoSmall} alt="logo" className={styles.logo} />}
             </Box>
             <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
