49878e389d8fc8940123959759e82a2141a30985
Samuel Carswell <samuelrcarswell@gmail.com>
2024-11-20 09:41:38 +1100

    AB#6587 Remove .xls from upload page

--- src/components/Upload/UploadMetadata.tsx
+++ src/components/Upload/UploadMetadata.tsx
@@ -60,10 +60,8 @@ function UploadInstructions({ setDrawerOpen }: any) {
       Metadata can be submitted in tabular format, either in CSV or Excel (xlsx) format.
       Files should have extensions
       <code>.csv </code>
-      ,
-      <code>.xlsx </code>
       or
-      <code>.xls </code>
+      <code>.xlsx </code>
       . If not using the proforma directly,
       ensure that column names in your CSV or Excel file match those in the proforma.
       <br />
@@ -226,10 +224,8 @@ function UploadMetadata() {
             Metadata can be submitted in tabular format, either in CSV or Excel (xlsx) format.
             Files should have extensions
             <code> .csv</code>
-            ,
-            <code> .xlsx </code>
             or
-            <code> .xls</code>
+            <code> .xlsx </code>
             . If not using the proforma directly,
             ensure that column names in your CSV or Excel file match those in the proforma.
           </Typography>
