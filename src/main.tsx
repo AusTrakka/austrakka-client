692b066990fc516de146c6f2d92ff727bef4b67f
Clare Sloggett <claresloggett@gmail.com>
2025-03-19 11:16:54 +1100

    Remove remaining MsalProvider; remove unused code

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { StyledEngineProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
-import { MsalProvider } from '@azure/msal-react';
 import { GlobalStyles } from '@mui/material';
 import { SnackbarProvider } from 'notistack';
 import App from './App';
-import { msalInstance } from './utilities/authUtils';
 import store from './app/store';
 import { globalStyles } from './assets/themes/theme';
 
@@ -18,16 +16,14 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <BrowserRouter>
       {/* StyledEngineProvider: Allows MUI's styles to be overridden */}
       <StyledEngineProvider injectFirst>
-        <MsalProvider instance={msalInstance}>
-          <GlobalStyles styles={globalStyles} />
-          <SnackbarProvider
-            autoHideDuration={5000}
-            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-            maxSnack={10}
-          >
-            <App />
-          </SnackbarProvider>
-        </MsalProvider>
+        <GlobalStyles styles={globalStyles} />
+        <SnackbarProvider
+          autoHideDuration={5000}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          maxSnack={10}
+        >
+          <App />
+        </SnackbarProvider>
       </StyledEngineProvider>
     </BrowserRouter>
   </Provider>,
--- src/permissions/roles.ts
+++ src/permissions/roles.ts
@@ -8,23 +8,3 @@ export enum RoleName {
   ResultWriter = 'ResultWriter',
 }
 
-// This information should perhaps be moved to server-side
-
-export const austrakkaRoles = [
-  RoleName.AusTrakkaAdmin,
-  RoleName.ProFormaEditor,
-];
-
-export const orgRoles = [
-  RoleName.Viewer,
-  RoleName.SeqViewer,
-  RoleName.Uploader,
-];
-
-export const projectRoles = [
-  RoleName.Viewer,
-  RoleName.SeqViewer,
-  RoleName.Uploader,
-  RoleName.ProjectAnalyst,
-  RoleName.ResultWriter,
-];
