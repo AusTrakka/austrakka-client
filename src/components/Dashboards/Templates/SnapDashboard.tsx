1606642acd14b797290930a7330f66e5fdd9e8eb
Clare Sloggett <claresloggett@gmail.com>
2025-03-24 22:28:31 +1100

    Add second column back to snap dashboard layout

--- src/components/Dashboards/Templates/SnapDashboard.tsx
+++ src/components/Dashboards/Templates/SnapDashboard.tsx
@@ -21,94 +21,108 @@ function SnapDashboard(props: ProjectDashboardTemplateProps) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container sx={{ alignItems: 'flex-start' }} spacing={2}>
-        <Grid size={12}>
-          <Card sx={cardStyle}>
-            <CardContent>
-              <SampleSummary
-                projectAbbrev={projectAbbrev}
-                filteredData={filteredData}
-                timeFilterObject={timeFilterObject}
-              />
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid size={12}>
-          <Card sx={cardStyle}>
-            <CardContent>
-              <EpiCurveChart
-                projectAbbrev={projectAbbrev}
-                filteredData={filteredData}
-                timeFilterObject={timeFilterObject}
-                preferredColourField="Country"
-              />
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid size={{ lg: 6, md: 12 }}>
-          <Card sx={tallCardStyle}>
-            <CardContent>
-              <MetadataCounts
-                projectAbbrev={projectAbbrev}
-                filteredData={filteredData}
-                timeFilterObject={timeFilterObject}
-                field="Date_coll"
-                title="Metadata counts"
-                categoryField="Country"
-              />
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid size={{ lg: 6, md: 12 }}>
-          <Card sx={tallCardStyle}>
-            <CardContent>
-              <HasSeq
-                projectAbbrev={projectAbbrev}
-                filteredData={filteredData}
-                timeFilterObject={timeFilterObject}
-                categoryField="Country"
-              />
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid container size={{ lg: 4, md: 6, sm: 12 }}>
-          <Card sx={tallCardStyle}>
-            <CardContent>
-              <MetadataValuePieChart
-                projectAbbrev={projectAbbrev}
-                filteredData={filteredData}
-                timeFilterObject={timeFilterObject}
-                field="mecA_status"
-                title="mecA status"
-              />
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid container size={{ lg: 4, md: 6, sm: 12 }}>
-          <Card sx={tallCardStyle}>
-            <CardContent>
-              <Counts
-                projectAbbrev={projectAbbrev}
-                filteredData={filteredData}
-                timeFilterObject={timeFilterObject}
-                field="SNAP_trial_group"
-                title="SNAP trial group counts"
-              />
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid container size={{ lg: 4, md: 6, sm: 12 }}>
-          <Card sx={tallCardStyle}>
-            <CardContent>
-              <Counts
-                projectAbbrev={projectAbbrev}
-                filteredData={filteredData}
-                timeFilterObject={timeFilterObject}
-                field="Referring_site"
-                title="Referring site counts"
-              />
-            </CardContent>
-          </Card>
-        </Grid>
+        <Grid
+          container
+          sx={{ alignItems: 'flex-start' }}
+          spacing={2}
+          size={{ lg: 8, md: 12 }}
+        > {/* big left column */}
+          <Grid size={12}>
+            <Card sx={cardStyle}>
+              <CardContent>
+                <SampleSummary
+                  projectAbbrev={projectAbbrev}
+                  filteredData={filteredData}
+                  timeFilterObject={timeFilterObject}
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={12}>
+            <Card sx={cardStyle}>
+              <CardContent>
+                <EpiCurveChart
+                  projectAbbrev={projectAbbrev}
+                  filteredData={filteredData}
+                  timeFilterObject={timeFilterObject}
+                  preferredColourField="Country"
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={{ lg: 6, md: 12 }}>
+            <Card sx={tallCardStyle}>
+              <CardContent>
+                <MetadataCounts
+                  projectAbbrev={projectAbbrev}
+                  filteredData={filteredData}
+                  timeFilterObject={timeFilterObject}
+                  field="Date_coll"
+                  title="Metadata counts"
+                  categoryField="Country"
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={{ lg: 6, md: 12 }}>
+            <Card sx={tallCardStyle}>
+              <CardContent>
+                <HasSeq
+                  projectAbbrev={projectAbbrev}
+                  filteredData={filteredData}
+                  timeFilterObject={timeFilterObject}
+                  categoryField="Country"
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid container size={{ lg: 6, md: 6, sm: 12 }}>
+            <Card sx={tallCardStyle}>
+              <CardContent>
+                <MetadataValuePieChart
+                  projectAbbrev={projectAbbrev}
+                  filteredData={filteredData}
+                  timeFilterObject={timeFilterObject}
+                  field="mecA_status"
+                  title="mecA status"
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid> {/* big left column */}
+        <Grid
+          container
+          sx={{ alignItems: 'flex-start' }}
+          spacing={2}
+          size={{ lg: 4, md: 12 }}
+        > {/* narrow right column */}
+          <Grid size={{ lg: 12, md: 6, sm: 12 }}> {/* NB at lg, 100% of narrower column */}
+            <Card sx={tallCardStyle}>
+              <CardContent>
+                <Counts
+                  projectAbbrev={projectAbbrev}
+                  filteredData={filteredData}
+                  timeFilterObject={timeFilterObject}
+                  field="SNAP_trial_group"
+                  title="SNAP trial group counts"
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={{ lg: 12, md: 6, sm: 12 }}>
+            <Card sx={tallCardStyle}>
+              <CardContent>
+                <Counts
+                  projectAbbrev={projectAbbrev}
+                  filteredData={filteredData}
+                  timeFilterObject={timeFilterObject}
+                  field="Referring_site"
+                  title="Referring site counts"
+                />
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid> {/* narrow right column */}
       </Grid>
     </Box>
   );
