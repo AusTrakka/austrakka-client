3cc1d94e3db95b9c71475a3ad3a9cedcfbfe37f2
Samuel Carswell <samuelrcarswell@gmail.com>
2025-05-09 13:31:17 +1000

    AB#6968 AB#6991 Use both ext and mimetype for upload input accept. Only validate ext after selection

--- src/components/Upload/FileDragDrop.tsx
+++ src/components/Upload/FileDragDrop.tsx
@@ -18,6 +18,10 @@ interface FileDragDropProps {
   setValidated: Dispatch<SetStateAction<boolean>>,
 }
 
+const getFileExtension = (f: File): string => {
+  return '.'+f.name.split('.').pop()
+}
+
 // eslint-disable-next-line react/function-component-definition
 const FileDragDrop: React.FC<FileDragDropProps> = (
   {
@@ -39,7 +43,7 @@ const FileDragDrop: React.FC<FileDragDropProps> = (
     const validateFilesAreOfType = {
       func: (_files: File[]) => ({
         success: Object.entries(validFormats).length === 0 ||
-          _files.every(f => Object.values(validFormats).includes(f?.type)),
+          _files.every(f => Object.keys(validFormats).includes(getFileExtension(f))),
         message: `All files must be of a valid format: ${Object.keys(validFormats).join(', ')}`,
       } as CustomUploadValidatorReturn),
     } as CustomUploadValidator;
@@ -166,7 +170,7 @@ const FileDragDrop: React.FC<FileDragDropProps> = (
                   onClick={onBrowseClick}
                   multiple={multiple}
                   onChange={handleBrowseChange}
-                  accept={Object.values(validFormats).toString()}
+                  accept={[...new Set(Object.entries(validFormats).flat().filter(x => x !== ""))].join(",")}
                   hidden
                 />
               </Button>
