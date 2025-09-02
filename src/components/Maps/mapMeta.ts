7e804776c444e7dbce45d245f475f0c93657204a
Mann Sheth <mann.sheth@unimelb.edu.au>
2025-09-02 14:28:09 +1000

    AB#7158 Establish a registry to map values to map

--- src/components/Maps/mapMeta.ts
+++ src/components/Maps/mapMeta.ts
@@ -5,11 +5,17 @@ import AUS_NZ from '../../assets/maps/au_nz_processed.json';
 export const Maps = {
   MALAYSIA: MALAYSIA as GeoJSON,
   AUS_NZ: AUS_NZ as GeoJSON,
+  WORLD: null,
+  // 1. Maybe an Australia only map?
+  // 2. I don't think New Zealand will need a standalone
+  // 3. Need to add a WorldMap [regions will not be hard to support with this one]
+  // 4. More to come...
 } as const;
 
 // Type that holds the correct values for the keys
 export type MapKey = keyof typeof Maps;
 export type MapJson = (typeof Maps)[MapKey];
+export type MapSupportInfo = [MapKey, boolean];
 
 export type MapFeatureWithStringProps = {
   properties: { [x: string]: string }
@@ -23,3 +29,27 @@ export const FeatureLookupField = {
 } as const;
 
 export type FeatureLookupFieldType = typeof FeatureLookupField[keyof typeof FeatureLookupField];
+
+export interface GeoCountRow {
+  geoFeature: string;
+  count: number;
+}
+
+type MapRegistryEntry = {
+  key: MapKey;
+  supports?: Set<string>; // country keys
+};
+
+export const MapRegistry: MapRegistryEntry[] = [
+  {
+    key: 'MALAYSIA',
+    supports: new Set(['MY', 'MYS']),
+  },
+  {
+    key: 'AUS_NZ',
+    supports: new Set(['AU', 'NZ', 'AUS', 'NZ']),
+  },
+  {
+    key: 'WORLD', // no supports needed, always included
+  },
+];
