57d0fce6243c1a5da81dd4888ec5dd7f2172002f
Clare Sloggett <claresloggett@gmail.com>
2026-03-06 22:55:15 +1100

    AB#7903 Make snackbar alerts clickable to dismiss; remove icons from snackbars

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import './index.css';
 import { StyledEngineProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import { MsalProvider } from '@azure/msal-react';
-import { GlobalStyles } from '@mui/material';
-import { SnackbarProvider } from 'notistack';
+import { Button, GlobalStyles } from '@mui/material';
+import { SnackbarProvider, closeSnackbar } from 'notistack';
 import App from './App';
 import { msalInstance } from './utilities/authUtils';
 import store from './app/store';
@@ -30,6 +30,22 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
               autoHideDuration={5000}
               anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
               maxSnack={10}
+              hideIconVariant
+              action={(key) => (
+                <Button
+                  onClick={() => closeSnackbar(key)}
+                  // an invisible button overlaying the whole snackbar
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                    background: 'transparent',
+                    color: 'transparent',
+                  }}
+                >Dismiss</Button>
+              )}
             >
               <App />
             </SnackbarProvider>
