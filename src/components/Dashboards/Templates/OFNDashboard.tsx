a06bff8320a911fe2c7df3951ef5b750a10705e9
Jolene Langevin <jolene.farrell@unimelb.edu.au>
2026-03-17 16:24:07 +1100

    AB#7866 Add back accidentally removed organisations widget

--- src/components/Dashboards/Templates/OFNDashboard.tsx
+++ src/components/Dashboards/Templates/OFNDashboard.tsx
@@ -1,9 +1,10 @@
 import { Box, Card, CardContent } from '@mui/material';
 import Grid from '@mui/material/Grid2';
-import { cardStyle } from '../../../styles/dashboardStyles';
+import { cardStyle, tallCardStyle } from '../../../styles/dashboardStyles';
 import type ProjectDashboardTemplateProps from '../../../types/projectdashboardtemplate.props.interface';
 import EpiCurveChart from '../../Widgets/ProjectWidgets/EpiCurveChart';
 import MetadataValueBarChart from '../../Widgets/ProjectWidgets/MetadataValueBarChart';
+import Organisations from '../../Widgets/ProjectWidgets/Organisations';
 import SampleSummary from '../../Widgets/ProjectWidgets/SampleSummary';
 
 function OFNDashboard(props: ProjectDashboardTemplateProps) {
@@ -25,31 +26,44 @@ function OFNDashboard(props: ProjectDashboardTemplateProps) {
             </Card>
           </Grid>
           <Grid container spacing={2} alignItems="stretch" size={{ sm: 12 }}>
-            <Grid size={{ xl: 6, lg: 6, md: 12, sm: 12 }}>
-              <Card sx={{ ...cardStyle, height: '100%' }}>
-                <CardContent>
-                  <MetadataValueBarChart
-                    projectAbbrev={projectAbbrev}
-                    filteredData={filteredData}
-                    timeFilterObject={timeFilterObject}
-                    field="Jurisdiction"
-                    title="Samples by jurisdiction"
-                    legendColumns={8}
-                  />
-                </CardContent>
-              </Card>
+            <Grid container direction="column" spacing={2} size={{ xl: 8, lg: 7, md: 12, sm: 12 }}>
+              <Grid size={{ sm: 12 }}>
+                <Card sx={{ ...cardStyle, height: '100%' }}>
+                  <CardContent>
+                    <MetadataValueBarChart
+                      projectAbbrev={projectAbbrev}
+                      filteredData={filteredData}
+                      timeFilterObject={timeFilterObject}
+                      field="Jurisdiction"
+                      title="Samples by jurisdiction"
+                      legendColumns={8}
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
+              <Grid size={{ sm: 12 }}>
+                <Card sx={{ ...cardStyle, height: '100%' }}>
+                  <CardContent>
+                    <MetadataValueBarChart
+                      projectAbbrev={projectAbbrev}
+                      filteredData={filteredData}
+                      timeFilterObject={timeFilterObject}
+                      field="cgMLST"
+                      title="Top 5 cgMLST"
+                      categoryLimit={5}
+                      legendColumns={5}
+                    />
+                  </CardContent>
+                </Card>
+              </Grid>
             </Grid>
-            <Grid size={{ xl: 6, lg: 6, md: 12, sm: 12 }}>
-              <Card sx={{ ...cardStyle, height: '100%' }}>
+            <Grid size={{ xl: 4, lg: 5, md: 12, sm: 12 }}>
+              <Card sx={{ ...tallCardStyle, height: '100%' }}>
                 <CardContent>
-                  <MetadataValueBarChart
+                  <Organisations
                     projectAbbrev={projectAbbrev}
                     filteredData={filteredData}
                     timeFilterObject={timeFilterObject}
-                    field="cgMLST"
-                    title="Top 5 cgMLST"
-                    categoryLimit={5}
-                    legendColumns={5}
                   />
                 </CardContent>
               </Card>
