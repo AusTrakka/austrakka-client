16752f6050778a5fe2dd33721ed67f89c008e01c
Samuel Carswell <samuelrcarswell@gmail.com>
2026-05-14 14:01:37 +1000

    disable minify

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ const customLogoDir: string = path.join(__dirname, 'src', 'assets', 'logos')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: false,
+  },
   plugins: [
     react(),
     ValidateEnv({
