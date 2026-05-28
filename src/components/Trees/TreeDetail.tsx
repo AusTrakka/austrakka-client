876c3191528c24e60ad1829d6d663a382de07ff4
Mann Sheth <mann.sheth@unimelb.edu.au>
2026-05-28 19:33:44 +1200

    AB#8146 Fix metadata loading logic for main tables and detail page
    For Project, Organisation, and Tree tables the field loading logic has been removed. Along with partial data checking. Now things only get decided on complete data load.

--- src/components/Trees/TreeDetail.tsx
+++ src/components/Trees/TreeDetail.tsx
@@ -358,7 +358,6 @@ function TreeDetail() {
           uniqueValues={projectMetadata?.fieldUniqueValues ?? null}
           tableMetadata={tableMetadata}
           metadataLoadingState={projectMetadata?.loadingState || MetadataLoadingState.IDLE}
-          fieldLoadingState={projectMetadata?.fieldLoadingStates || {}}
           emptyColumns={projectMetadata?.emptyColumns || []}
           treeName={tree.treeName}
         />
@@ -548,10 +547,7 @@ function TreeDetail() {
   };
 
   const renderWarning = () => {
-    if (
-      projectMetadata?.loadingState === MetadataLoadingState.ERROR ||
-      projectMetadata?.loadingState === MetadataLoadingState.PARTIAL_LOAD_ERROR
-    ) {
+    if (projectMetadata?.loadingState === MetadataLoadingState.ERROR) {
       return (
         <Alert severity="error">
           <AlertTitle>Error</AlertTitle>
