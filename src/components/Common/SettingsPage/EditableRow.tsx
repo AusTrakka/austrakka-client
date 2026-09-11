369b9b6b5bbe89594f10d05ff13ff71efbecccce
Oli Jacobs <oliver.jacobs@unimelb.edu.au>
2026-09-11 15:34:00 +1000

    AB#8435 satisfy biome

--- src/components/Common/SettingsPage/EditableRow.tsx
+++ src/components/Common/SettingsPage/EditableRow.tsx
@@ -71,8 +71,8 @@ function EditableRow(props: EditableRowProps) {
       throw new Error('Organisations cannot be null');
     }
     if (!selectedOrg) {
-        // Not an error, the user can clear the autocomplete field and cause this
-        return;
+      // Not an error, the user can clear the autocomplete field and cause this
+      return;
     }
     setEditedValues((prevValues) => {
       if (!prevValues) return null;
